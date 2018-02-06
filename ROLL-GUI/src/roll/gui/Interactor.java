945ae8356950d79bd81879c15c32f4bccfbe5a5f
Clexma <lixie15@mails.ucas.ac.cn>
2018-02-06 21:31:58 +0800

    front to startLearning connected

--- ROLL-GUI/src/roll/gui/Interactor.java
+++ ROLL-GUI/src/roll/gui/Interactor.java
@@ -4,10 +4,28 @@ public class Interactor {
 	private Algorithm a;
 	private Approach push;
 	private DataStructure ds;
+	private String[] alphabetLetters;
+	private Integer alphabetNumber;
+	
 	public Interactor() {
-		
+		this.a = Algorithm.PERIODIC;
+		this.push = Approach.UNDER;
+		this.ds = DataStructure.TABLE;
 	}
-	public static void startLearning() {
-		
+	
+	public void assignValue(Integer num, String[] letters, Algorithm ago, Approach ap, DataStructure dataStruct) {
+		this.alphabetNumber = num;
+		this.alphabetLetters = new String[num];
+		for(int i = 0; i < num; i++) {
+			this.alphabetLetters[i] = letters[i];
+		}
+		this.a = ago;
+		this.push = ap;
+		this.ds = dataStruct;
+		System.out.println("assign end");
+	}
+	
+	public String startLearning() {
+		return this.a.toString() + " " + this.push.toString() + " " + this.ds.toString() + " " + this.alphabetNumber;
 	}
 }
--- ROLL-GUI/src/roll/gui/JavaScriptBridge.java
+++ ROLL-GUI/src/roll/gui/JavaScriptBridge.java
@@ -4,6 +4,13 @@ import javafx.application.Platform;
 import javafx.scene.layout.Region;
 
 public class JavaScriptBridge extends Region {
+	private Interactor interactor;
+	
+	
+	public JavaScriptBridge() {
+		this.interactor = new Interactor();
+	}
+	
 	public void callback(String data) {
 		Platform.exit();
 	}
@@ -12,7 +19,60 @@ public class JavaScriptBridge extends Region {
 		System.out.println(input);
 	}
 	
-	public String getStringFromJava() {
-		return "Start Learning";
+	public String submitOptions(String alphaNum, String alphabet, String dataStructure, String algorithm, String approach) {
+//		System.out.println(alphaNum);
+//		System.out.println(alphabet);
+//		System.out.println(dataStructure);
+//		System.out.println(algorithm);
+//		System.out.println(approach);
+		Algorithm a = Algorithm.PERIODIC;
+		Approach push = Approach.UNDER;
+		DataStructure ds = DataStructure.TABLE;
+		Integer alphabetNum = Integer.parseInt(alphaNum);
+		String[] alphabetLetters = new String[alphabetNum];
+		alphabetLetters = alphabet.split("\\,");
+		for(int i = 0; i < alphabetNum; i++) {
+			alphabetLetters[i] = alphabetLetters[i].trim();
+			System.out.println(alphabetLetters[i]);
+		}
+		if(dataStructure.equals("tree")) {
+			ds = DataStructure.TREE;
+		} else if(dataStructure.equals("table")) {
+			ds = DataStructure.TABLE;
+		} else {
+			assert(false);
+		}
+		
+		if(algorithm.equals("periodic")) {
+			a = Algorithm.PERIODIC;
+		} else if(algorithm.equals("syntactic")) {
+			a = Algorithm.SYNTACTIC;
+		} else if(algorithm.equals("recurrent")) {
+			a = Algorithm.RECURRENT;
+		} else if(algorithm.equals("ldollar")) {
+			a = Algorithm.LDOLLAR;
+		} else {
+			assert(false);
+		}
+		
+		if(approach.equals("under")) {
+			push = Approach.UNDER;
+		} else if(approach.equals("over")) {
+			push = Approach.OVER;
+		} else {
+			assert(false);
+		}
+		System.out.println("assign begin");
+
+		this.interactor.assignValue(alphabetNum, alphabetLetters, a, push, ds);
+		System.out.println("submit return");
+		return "Start Okay";
 	}
+	
+	public String startLearning() {
+		System.out.println("startLearning");
+		return this.interactor.startLearning();
+	}
+	
+	
 }
