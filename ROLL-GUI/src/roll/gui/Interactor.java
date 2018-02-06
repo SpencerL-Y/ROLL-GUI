b77f5aa5fe56d2e0c440d018f701f28175913be9
Clexma <lixie15@mails.ucas.ac.cn>
2018-02-06 20:25:17 +0800

    Add interactor

---
+++ ROLL-GUI/src/roll/gui/Interactor.java
@@ -0,0 +1,13 @@
+package roll.gui;
+
+public class Interactor {
+	private Algorithm a;
+	private Approach push;
+	private DataStructure ds;
+	public Interactor() {
+		
+	}
+	public static void startLearning() {
+		
+	}
+}
